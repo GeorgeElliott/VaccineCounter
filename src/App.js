97a67b5e870e49fdeb943ef4734066c2c8962b2f
George Elliott <georgeelliott93@gmail.com>
2021-03-05 21:51:40 +0000

    Full Doses (#8)
    - Added ability to choose to view full doses
    - Added information modal
    - Cleaned up some of the html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Loader from "./Loader";
+import Modal from "./Modal";
 
 class App extends React.Component {
   state = {
@@ -9,52 +10,63 @@ class App extends React.Component {
       {
         code: "uk",
         population: 68123219,
-        vaccineCount: 20275451,
-        dateUpdated: new Date(2021, 2, 1, 16, 0, 0, 0),
-        dailySevenDayAverage: 364515.9,
+        vaccineCount: 20982571,
+        vaccineCountFullDose: 963862,
+        dateUpdated: new Date(2021, 2, 5, 16, 0, 0, 0),
+        dailySevenDayAverage: 327248,
+        dailySevenDayAverageFullDose: 37592,
         formattedName: "the United Kingdom",
-        minUpdateInterval: 4000,
-        maxUpdateInterval: 6020,
+        buttonText: "U.K.",
       },
       {
         code: "wal",
         population: 3150000,
-        vaccineCount: 925669,
-        dateUpdated: new Date(2021, 2, 1, 16, 0, 0, 0),
-        dailySevenDayAverage: 9060.1,
+        vaccineCount: 951540,
+        vaccineCountFullDose: 139445,
+        dateUpdated: new Date(2021, 2, 5, 16, 0, 0, 0),
+        dailySevenDayAverage: 8895.7,
+        dailySevenDayAverageFullDose: 9942,
         formattedName: "Wales",
-
+        buttonText: "Wales",
       },
       {
         code: "eng",
         population: 56290000,
-        vaccineCount: 17212804,
-        dateUpdated: new Date(2021, 2, 1, 16, 0, 0, 0),
-        dailySevenDayAverage: 322104.3,
+        vaccineCount: 17785702,
+        vaccineCountFullDose: 684220,
+        dateUpdated: new Date(2021, 2, 5, 16, 0, 0, 0),
+        dailySevenDayAverage: 284387.1,
+        dailySevenDayAverageFullDose: 20325.9,
         formattedName: "England",
-        minUpdateInterval: 3000,
-        maxUpdateInterval: 4700,
+        buttonText: "England",
       },
       {
         code: "nir",
         population: 1890000,
-        vaccineCount: 525400,
-        dateUpdated: new Date(2021, 2, 1, 16, 0, 0, 0),
-        dailySevenDayAverage: 9624.3,
+        vaccineCount: 556721,
+        vaccineCountFullDose: 40139,
+        dateUpdated: new Date(2021, 2, 5, 16, 0, 0, 0),
+        dailySevenDayAverage: 9304,
+        dailySevenDayAverageFullDose: 1214.6,
         formattedName: "Northern Ireland",
+        buttonText: "N.Ireland",
       },
       {
         code: "scot",
         population: 5460000,
-        vaccineCount: 1611578,
-        dateUpdated: new Date(2021, 2, 1, 16, 0, 0, 0),
-        dailySevenDayAverage: 23727.1,
+        vaccineCount: 1688608,
+        vaccineCountFullDose: 100058,
+        dateUpdated: new Date(2021, 2, 5, 16, 0, 0, 0),
+        dailySevenDayAverage: 24661.1,
+        dailySevenDayAverageFullDose: 6199.6,
         formattedName: "Scotland",
+        buttonText: "Scotland",
       },
     ],
     activeCountry: {},
     valueToAdd: null,
     percentageOfPopulation: null,
+    fullDoseToggled: false,
   };
 
   componentDidMount() {
@@ -72,7 +84,7 @@ class App extends React.Component {
   }
 
   getFormattedTime() {
-    if (this.state.activeCountry.dateUpdated) {
+    if (this.state.activeCountry && this.state.activeCountry.dateUpdated) {
       return (
         <div>{`Government data from ${this.state.activeCountry.dateUpdated.toDateString()} at ${this.state.activeCountry.dateUpdated.toTimeString()}`}</div>
       );
@@ -84,27 +96,21 @@ class App extends React.Component {
   renderContent() {
     if (!this.state.loading) {
       return (
-        <div>
+        <div className="relative">
+          <Modal formattedTime={this.getFormattedTime}/>
           <h2 className="text-center text-6xl leading-loose md:text-8xl py-12">
             {Math.floor(this.state.vaccineCount).toLocaleString()}
           </h2>
-          <div className="text-center mx-6 py-2">
-            People in {this.state.activeCountry.formattedName} have received
-            their first dose of Covid-19 vaccine (estimated).
+          <div className="text-center text-sm mx-6 py-2">
+            People in {this.state.activeCountry.formattedName} have received{" "}
+            {!this.state.fullDoseToggled
+              ? "their first dose"
+              : "all required doses"}
+            of the Covid-19 vaccine (estimated).
           </div>
-          <div className="text-center mx-6 py-2">
+          <div className="text-center text-sm mx-6 py-2">
             That is roughly {this.state.percentageOfPopulation.toFixed(2)}% of{" "}
             {this.state.activeCountry.formattedName}'s population.
-          </div>
-          <div className="text-center mx-6 py-2">
-            <a
-              className="underline"
-              target="_blank"
-              rel="noreferrer"
-              href="https://coronavirus.data.gov.uk/details/vaccinations"
-            >
-              We get our estimate using vaccination data from here.
-            </a>
           </div>
         </div>
       );
@@ -117,28 +123,39 @@ class App extends React.Component {
     return (
       <div>
         <header className="w-full mx-auto p-4 md:absolute">
-          <NavBar toggleCountry={this.changeActiveCountry} />
+          <NavBar
+            countries={this.state.countries}
+            activeCountry={
+              this.state.activeCountry ? this.state.activeCountry.code : "uk"
+            }
+            toggleCountry={this.changeActiveCountry}
+            toggleFullDose={this.toggleFullDose}
+          />
         </header>
         <main className="flex md:h-screen h-full flex-row justify-center items-center flex-grow">
           {this.renderContent()}
         </main>
-        <footer className="md:absolute relative bottom-0 w-full p-4 sm:pt-10 sm:pb-4 sm:px-4 mx-auto text-center">
+        <footer className="md:absolute bottom-0 w-full p-4 md:pt-10 mx-auto text-center">
           {this.getFormattedTime()}
         </footer>
       </div>
     );
   }
 
-  setActiveCountry(activeCountry) {
+  setActiveCountry(activeCountry, isFullDose = false) {
     this.setState({
       loading: true,
     });
-    var dateDiff = new Date().getTime() - activeCountry.dateUpdated.getTime();
-    var secondsBetween = Math.abs(dateDiff) / 1000;
-    var vaccinesPerSecond = activeCountry.dailySevenDayAverage / 86400;
-    const vaccineCount =
-      activeCountry.vaccineCount + secondsBetween * vaccinesPerSecond;
-
+    const dateDiff = new Date().getTime() - activeCountry.dateUpdated.getTime();
+    const secondsBetween = Math.abs(dateDiff) / 1000;
+    const vaccinesPerSecond =
+      (!isFullDose
+        ? activeCountry.dailySevenDayAverage
+        : activeCountry.dailySevenDayAverageFullDose) / 86400;
+    const baseVaccineCount = !isFullDose
+      ? activeCountry.vaccineCount
+      : activeCountry.vaccineCountFullDose;
+    const vaccineCount = baseVaccineCount + secondsBetween * vaccinesPerSecond;
     this.setState({
       activeCountry: activeCountry,
       valueToAdd: vaccinesPerSecond,
@@ -147,36 +164,18 @@ class App extends React.Component {
       loading: false,
     });
 
-    var interval = activeCountry.code === "uk" || activeCountry.code === "eng" ?
-      Math.floor(Math.random() * (activeCountry.maxUpdateInterval - activeCountry.minUpdateInterval)) + activeCountry.minUpdateInterval :
-      1000;
-
-    this.totalInterval = setInterval(
-      () => {
-        interval = Math.floor(Math.random() * (activeCountry.maxUpdateInterval - activeCountry.minUpdateInterval)) + activeCountry.minUpdateInterval;
-        if(vaccinesPerSecond > 3){
-          var now = new Date();
-          var future = new Date(now.getTime() + interval);
-          let dateDiff = future.getTime() - now.getTime();
-          var secondsBetween = Math.abs(dateDiff) / 1000;
-          this.animateValue(this.state.vaccineCount, this.state.vaccineCount + secondsBetween * vaccinesPerSecond, 500);
-        }
-        else
-        {
-          interval = 1000;
-          this.setState({
-            vaccineCount: this.state.vaccineCount + this.state.valueToAdd,
-          });
-        }
-      },
-      interval
-    );
+    this.totalInterval = setInterval(() => {
+      this.setState({
+        vaccineCount: this.state.vaccineCount + this.state.valueToAdd,
+      });
+    }, 1000);
 
     this.percentInterval = setInterval(
       () =>
         this.setState({
           percentageOfPopulation:
-            (this.state.vaccineCount / activeCountry.population) * 100,
+            (this.state.vaccineCount / this.state.activeCountry.population) *
+            100,
         }),
       10000
     );
@@ -184,23 +183,18 @@ class App extends React.Component {
 
   changeActiveCountry = (code) => {
     this.stopIntervals();
-    this.setActiveCountry(this.state.countries.find((x) => x.code === code));
+    this.setActiveCountry(
+      this.state.countries.find((x) => x.code === code),
+      this.state.fullDoseToggled
+    );
   };
 
-  animateValue(start, end, duration) {
-    let startTimestamp = null;
-    const step = (timestamp) => {
-      if (!startTimestamp) startTimestamp = timestamp;
-      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      this.setState({
-        vaccineCount: Math.floor(progress * (end - start) + start),
-      });
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      }
-    };
-    window.requestAnimationFrame(step);
-  }
+  toggleFullDose = (isToggled) => {
+    this.setState({
+      fullDoseToggled: isToggled,
+    });
+    this.setActiveCountry(this.state.activeCountry, isToggled);
+  };
 }
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,112 @@
+import React from "react";
+
+export default function Modal(props) {
+  const [showModal, setShowModal] = React.useState(false);
+
+  return (
+    <div className="sm:relative">
+      <div
+        className="absolute md:top-2 md:right-2 top-2 right-7 cursor-pointer hover:animate-pulse"
+        onClick={() => setShowModal(true)}
+      >
+        <svg
+          className="w-5 h-5 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto md:fixed md:inset-0 left:0 absolute z-50 outline-none focus:outline-none"
+            onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3xl text-gray-600  font-semibold uppercase">
+                    More information
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative px-6 py-3 flex-auto my-4 text-gray-600  leading-relaxed">
+                  <h2 className="text-2xl pb-3 font-bold underline">Why?</h2>
+                  <p>
+                    I have been doing a react course and I wanted to take some
+                    of what I have learnt and put it to use. Covid-19
+                    vaccinations have been going on at an impressive rate around
+                    the world and I thought that this would be a fun way to show
+                    the pace that vaccications are happening in the U.K.
+                  </p>
+                  <h2 className="text-2xl py-3 font-bold underline">Data</h2>
+                  <p>
+                    I use data from{" "}
+                    <a
+                      className="underline"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://coronavirus.data.gov.uk/details/vaccinations"
+                    >
+                      United Kingdom's Covid-19 Dashboard
+                    </a>
+                    . The way I am calculating the data is probably not the most
+                    accurate but this was for a bit of fun. I take the amount of
+                    cases at the end of the previous day. Then use the seven day
+                    average and the difference in time between the stats being
+                    released and the current time to work out how many people
+                    are being vaccinated per second. This is not perfect because
+                    vaccines are not happening at the same rate at all hours of
+                    the day.
+                  </p>
+                  <h2 className="text-2xl py-3 font-bold underline">
+                    Future Changes
+                  </h2>
+                  <p>Here are a list of things I am thinking of adding.</p>
+                  <ul className=" py-3 list-disc list-inside">
+                    <li className="line-through">
+                      Full Dosing I am currently only including information
+                      about the first dose.
+                    </li>
+                    <li>
+                      Other countries apart from the U.K. Maybe the U.S.A. or
+                      the whole of the E.U.
+                    </li>
+                    <li>
+                      Looking into getting the data from API's so I do not have
+                      to update the data manually.
+                    </li>
+                  </ul>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+                  <button
+                    className="rounded px-3 py-2 m-1 bg-blue-700 uppercase"
+                    type="button"
+                    style={{ transition: "all .15s ease" }}
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </div>
+  );
+}
