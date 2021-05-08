194edf52dd76e8d50c179900d2ec939aedd2d7b5
george <georgeelliott93@gmail.com>
2021-05-08 19:33:54 +0100

    Fix issues with dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {
-      dropShadows: {
-        "drop-shadow-lg-yellow-300" : "--tw-drop-shadow: drop-shadow(0 10px 8px rgba(252,211,77, 0.04)) drop-shadow(0 4px 3px rgba(252,211,77, 0.1))"
-      }
-    },
+    extend: {},
   },
   variants: {
     extend: {
